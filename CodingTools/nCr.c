4282dce2b0e992019c396c8cba797697079594a4
Piyush Jain <piyushjain1sa@gmail.com>
2017-10-15 02:49:50 +0530

    Update nCr.c

--- CodingTools/nCr.c
+++ CodingTools/nCr.c
@@ -23,6 +23,7 @@ void pre()
     {
         fact[i] = (1LL * fact[i - 1] * i) % mod;
     }
+    ifact[N - 1] = 977041169;   // multiplicative modulo inverse of (N-1)!
     ifact[N - 1] = 977041169;
     for(ll i = N - 2 ; i >= 0 ; --i)
     {
